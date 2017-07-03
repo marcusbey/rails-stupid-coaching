3bc2b9366526e9f325be5c2f358b590dba77d3c2
Romain BOBOE <rboboe@gmail.com>
2017-07-03 14:46:40 -0400

    ask to you coack

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+
+  get 'answer', to: 'coachings#answer'
+
+  get 'ask', to: 'coachings#ask'
+  get 'root', to: 'coachings#ask'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
